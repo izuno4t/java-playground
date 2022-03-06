599872347d5e6a62275fd720d74bffbcc0b6d451
izuno4t <9846+izuno4t@users.noreply.github.com>
2022-03-06 10:28:02 +0900

    add NativeHttpClient Usage

--- src/main/java/com/example/api/client/NativeHttpClient.java
+++ src/main/java/com/example/api/client/NativeHttpClient.java
@@ -6,29 +6,48 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import static java.net.http.HttpRequest.Builder;
+import static java.net.http.HttpRequest.BodyPublishers;
 import static java.net.http.HttpRequest.newBuilder;
 
 public class NativeHttpClient {
 
     private static final Logger logger = LoggerFactory.getLogger(NativeHttpClient.class);
 
-    private static final String SERVER_1 = "https://httpbin.org/";
+    private final String baseUrl;
 
     private static final HttpClient httpClient = HttpClient.newHttpClient();
 
-    public void request() throws IOException, InterruptedException {
-        for (int i = 0; i < 11; i++) {
-            Builder builder = newBuilder().uri(URI.create(SERVER_1 + "/get?q=" + i))
-                    .GET();
-            HttpRequest request = builder.build();
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            logger.info("status code:{}", response.statusCode());
-            logger.info("body: {}", response.body());
-
-        }
+    public NativeHttpClient(String baseUrl) {
+        this.baseUrl = baseUrl;
     }
+
+    public void request() throws IOException, InterruptedException {
+        var requestBody1 = """
+                {
+                    "foo":"bar1"
+                }
+                """;
+        post(requestBody1, "1");
+        var requestBody2 = """
+                {
+                    "foo":"bar2"
+                }
+                """;
+        post(requestBody2, "2");
+        
+    }
+
+    public void post(String requestBody, String seq) throws IOException, InterruptedException {
+        var request = newBuilder(URI.create(baseUrl + "/post?seq=" + seq))
+                .header("Content-Type", "application/json")
+                .POST(BodyPublishers.ofString(requestBody))
+                .build();
+
+        var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        logger.info("status code:{}", response.statusCode());
+    }
+
+
 }
--- src/test/java/com/example/api/client/NativeHttpClientTest.java
+++ src/test/java/com/example/api/client/NativeHttpClientTest.java
@@ -1,23 +1,93 @@
 package com.example.api.client;
 
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class NativeHttpClientTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(NativeHttpClientTest.class);
+
+    MockWebServer server;
+
+    String baseUrl;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws IOException {
+        server = new MockWebServer();
+        server.start();
+        baseUrl = StringUtils.chop(server.url("").toString());
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+    @AfterEach
+    void tearDown() throws IOException {
+        server.shutdown();
     }
 
 
-    @org.junit.jupiter.api.Test
+    @Test
     void testRequest() throws IOException, InterruptedException {
-        var client = new NativeHttpClient();
+
+        var responseBody = """
+                {
+                  "args": {},
+                  "headers": {
+                    "Accept": "application/json",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Accept-Language": "ja,en-US;q=0.9,en;q=0.8",
+                    "Dnt": "1",
+                    "Host": "httpbin.org",
+                    "Referer": "https://httpbin.org/",
+                    "Sec-Ch-Ua": "\\" Not A;Brand\\";v=\\"99\\", \\"Chromium\\";v=\\"98\\", \\"Google Chrome\\";v=\\"98\\"",
+                    "Sec-Ch-Ua-Mobile": "?0",
+                    "Sec-Ch-Ua-Platform": "\\"macOS\\"",
+                    "Sec-Fetch-Dest": "empty",
+                    "Sec-Fetch-Mode": "cors",
+                    "Sec-Fetch-Site": "same-origin",
+                    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36",
+                    "X-Amzn-Trace-Id": "Root=1-6223f986-3f1e8c56147a08d244510c41"
+                  },
+                  "origin": "121.111.182.24",
+                  "url": "https://httpbin.org/get"
+                }
+                """;
+
+        server.enqueue(new MockResponse().setBody(responseBody));
+        server.enqueue(new MockResponse().setBody(responseBody));
+
+
+        var client = new NativeHttpClient(baseUrl);
         client.request();
+
+        // request 1回目
+        var request1 = server.takeRequest();
+        logger.info("1回目のリクエスト: {}", request1.getRequestUrl());
+        assertThat(request1.getRequestUrl()).hasToString(baseUrl + "/post?seq=1");
+        assertThat(request1.getBody().readString(StandardCharsets.UTF_8)).isEqualTo("""
+                {
+                    "foo":"bar1"
+                }
+                """);
+
+        // request 2回目
+        var request2 = server.takeRequest();
+        logger.info("2回目のリクエスト: {}", request2.getRequestUrl());
+        assertThat(request2.getRequestUrl()).hasToString(baseUrl + "/post?seq=2");
+        assertThat(request2.getBody().readString(StandardCharsets.UTF_8)).isEqualTo("""
+                {
+                    "foo":"bar2"
+                }
+                """);
+
     }
 }
