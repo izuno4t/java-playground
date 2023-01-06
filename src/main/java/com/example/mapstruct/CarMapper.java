2f6afb4d66d949edf0c8d6d16da991ef01d5e59a
izuno4t <9846+izuno4t@users.noreply.github.com>
2023-01-06 18:09:21 +0900

    I got it.

--- src/main/java/com/example/mapstruct/CarMapper.java
+++ src/main/java/com/example/mapstruct/CarMapper.java
@@ -1,13 +1,14 @@
 package com.example.mapstruct;
 
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface CarMapper {
 
     CarMapper INSTANCE = Mappers.getMapper(CarMapper.class);
 
     @Mapping(source = "numberOfSeats", target = "seatCount")
-    @Mapping(source = "make", target = "makes")
     CarDto carToCarDto(Car car);
 }
